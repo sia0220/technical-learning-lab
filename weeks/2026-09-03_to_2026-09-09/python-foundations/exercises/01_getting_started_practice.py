17559ec8731a74d92267650d38a3a4d75b1d020b
sia0220 <aliciand@umich.edu>
2026-09-07 21:40:30 -0400

    Add Python getting started practice

---
+++ weeks/2026-09-03_to_2026-09-09/python-foundations/exercises/01_getting_started_practice.py
@@ -0,0 +1,16 @@
+# Include: heading printed with print(), two sentences, two arithmetic calculations, and at least once comment 
+heading = "HEADING"
+print(heading)
+
+# Prints a line made of dashes with the same number of dashes as the length of the heading.
+print("-" * len(heading))
+
+print() # Prints an exmpty line.
+print("This is a sentence.")
+print("This is another sentence.")
+
+x = 3
+y = 2
+
+print(x,"+",y,"=",x + y)
+print(x,"*",y,"=",x * y)
